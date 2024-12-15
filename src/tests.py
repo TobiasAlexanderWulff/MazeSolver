12b9c59487ffd6caaa2eac2895d3d88231322a15
Tobias Alexander Wulff <tobias.wulff@live.de>
2024-12-15 15:13:13 +0100

    add unittests

---
+++ src/tests.py
@@ -0,0 +1,30 @@
+import unittest
+
+from maze import Maze
+from cell import Cell
+
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+    
+    def test_maze_filled_with_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for i in range(num_cols):
+            for j in range(num_rows):
+                self.assertIsInstance(m1._cells[i][j], Cell)
+
+if __name__ == "__main__":
+    unittest.main()
