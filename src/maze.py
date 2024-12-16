5b887a6542afdda9f8003eb858187f2136e90ce5
Tobias Alexander Wulff <tobias.wulff@live.de>
2024-12-16 15:34:09 +0100

    add breaking cell walls recursivly

--- src/maze.py
+++ src/maze.py
@@ -30,11 +30,12 @@ class Maze:
         self._create_cells()
     
     def _create_cells(self):
+        print("generating maze..")
         self._cells = [[Cell(self._win) for _ in range(self._num_rows)] for _ in range(self._num_cols)]        
         self._break_entrance_and_exit()
-        for i in range(self._num_cols):
-            for j in range(self._num_rows):
-                self._draw_cell(i, j)
+        self._break_walls_r(0, 0)
+        print("generation completed!")
+        
     
     def _draw_cell(self, i, j):
         if self._win is None:
@@ -55,3 +56,42 @@ class Maze:
     def _break_entrance_and_exit(self):
         self._cells[0][0].has_top_wall = False
         self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+
+    def _break_walls_r(self, i, j):
+        c = self._cells[i][j]
+        c.visited = True
+        while True:            
+            
+            available_directions = []
+            if i != 0 and not self._cells[i - 1][j].visited:
+                available_directions.append("L")
+            if j != 0 and not self._cells[i][j - 1].visited:
+                available_directions.append("U")
+            if i != self._num_cols - 1 and not self._cells[i + 1][j].visited:
+                available_directions.append("R")
+            if j != self._num_rows - 1 and not self._cells[i][j + 1].visited:
+                available_directions.append("D")
+                
+            if len(available_directions) == 0:
+                self._draw_cell(i, j)
+                return
+            
+            direction = available_directions[random.randint(0, len(available_directions) - 1)]
+            
+            match direction:
+                case "L":
+                    c.has_left_wall = False
+                    self._cells[i - 1][j].has_right_wall = False
+                    self._break_walls_r(i - 1, j)
+                case "U":
+                    c.has_top_wall = False
+                    self._cells[i][j - 1].has_bottom_wall = False
+                    self._break_walls_r(i, j - 1)
+                case "R":
+                    c.has_right_wall = False
+                    self._cells[i + 1][j].has_left_wall = False
+                    self._break_walls_r(i + 1, j)
+                case "D":
+                    c.has_bottom_wall = False
+                    self._cells[i][j + 1].has_top_wall = False
+                    self._break_walls_r(i, j + 1)
